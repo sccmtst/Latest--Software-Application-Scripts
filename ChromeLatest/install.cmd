e4163c7ce9b5101f990c66c6c1a0f521bb121b13
SCCMTST.COM <36771799+sccmtst@users.noreply.github.com>
2022-04-19 11:53:24 -0400

    Update install.cmd

--- ChromeLatest/install.cmd
+++ ChromeLatest/install.cmd
@@ -2,4 +2,4 @@
 cd /d %~dp0
 SET val=%1
 IF [%1] EQU [] SET val=INSTALL
-PowerShell.exe -ExecutionPolicy Bypass -NoProfile -Command ".\Install.ps1 -Action %val%"
+PowerShell.exe -ExecutionPolicy Bypass -NoProfile -Command ".\app.ps1 -Action %val%"
