236f494dbdddba8c700267897f8938899f3db832
SCCMTST.COM <36771799+sccmtst@users.noreply.github.com>
2023-01-29 19:26:49 -0500

    Update app.ps1
    updated script to run faster

--- SlackLatest/app.ps1
+++ SlackLatest/app.ps1
@@ -9,9 +9,11 @@ IF (Test-Path "$ENV:SystemDrive\Program files (x86)"){
     $URL = 'https://slack.com/ssb/download-win-msi'
     $BIT = "32"
 }
+
 Write-Host "$($Action + "ing") Slack Please Wait..."
+$Script:ProgressPreference = "SilentlyContinue"
 $FileName = "Slack_" + $BIT + ".msi"
-Get-Process slack -ErrorAction SilentlyContinue | Stop-Process -Force 
+Get-Process slack -ErrorAction SilentlyContinue | Stop-Process -Force
 
 function Install-App {
     try {
@@ -23,26 +25,23 @@ function Install-App {
         $Install = (Get-ChildItem | Where-Object -Property Name -like "Backup*$BIT.msi").Name
     }
     $Pros = Start-Process msiexec.exe -ArgumentList "/i $Install /QN /l* $ENV:Temp\Slack_Install.log" -Wait -PassThru
-    IF ($($Pros.ExitCode) -ne 0){EXIT $Pros}
+    $StopCode = $($Pros.ExitCode)
 }
 
 Function Uninstall-App {
-    #Google Keeps switching there uninstall string location this will find it no matter what 
+    #Google Keeps switching there uninstall string location this will find it no matter what
     $String = (Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | Get-Item | Get-ItemProperty | Where-Object -Property DisplayName -like "*Slack (Machine)*").UninstallString
     if ($String -eq $null) {$String = (Get-ChildItem -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | Get-Item | Get-ItemProperty | Where-Object -Property DisplayName -like "*Slack (Machine)*").UninstallString}
     if ($String -eq $null) {Write-Host "Slack is not installed" ; EXIT 0}
     $CODE = $String.Substring($($String.IndexOf("{")))
     $Pros = Start-Process msiexec.exe -ArgumentList "/X $CODE /QN /NORESTART /l* $ENV:Temp\Slack_Uninstall.log" -Wait -PassThru
-    IF ($($Pros.ExitCode) -ne 0){EXIT $Pros}
-}
-
-Function Repair-App {
-    Uninstall-App
-    Install-App
+    $StopCode = $($Pros.ExitCode)
 }
 
 switch ($Action) {
     'Uninstall' {Uninstall-App}
-    'Repair' {Repair-App}
+    'Repair' {Uninstall-App;Install-App}
     Default {Install-app}
 }
+
+EXIT $StopCode
