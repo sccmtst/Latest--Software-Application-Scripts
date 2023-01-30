48825cd6b7e485c7e5651a5f1e87674d8f2b12c6
SCCMTST.COM <36771799+sccmtst@users.noreply.github.com>
2023-01-29 19:25:52 -0500

    Update app.ps1
    updated script to run faster

--- FireFoxLatest/app.ps1
+++ FireFoxLatest/app.ps1
@@ -9,9 +9,11 @@ IF (Test-Path "$ENV:SystemDrive\Program files (x86)"){
     $URL = 'https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win&lang=#language'
     $BIT = "32"
 }
+
 Write-Host "$($Action + "ing") Mozilla Firefox Please Wait..."
 $FileName = "Mozilla_Firefox_" + $BIT + ".msi"
-Get-Process firefox -ErrorAction SilentlyContinue | Stop-Process -Force 
+$Script:ProgressPreference = "SilentlyContinue"
+Get-Process firefox -ErrorAction SilentlyContinue | Stop-Process -Force
 
 function Install-App {
     try {
@@ -23,25 +25,21 @@ function Install-App {
         $Install = (Get-ChildItem | Where-Object -Property Name -like "Backup*$BIT.msi").Name
     }
     $Pros = Start-Process msiexec.exe -ArgumentList "/i $Install /QN /l* $ENV:Temp\Mozilla_Firefox_Install.log" -Wait -PassThru
-    IF ($Pros.ExitCode -ne 0){EXIT $Pros}
+    $StopCode = $($Pros.ExitCode)
 }
 
 Function Uninstall-App {
-    #Google Keeps switching there uninstall string location this will find it no matter what 
     $String = (Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | Get-Item | Get-ItemProperty | Where-Object -Property DisplayName -like "Mozilla Firefox*").UninstallString
     if ($String -eq $null) {$String = (Get-ChildItem -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | Get-Item | Get-ItemProperty | Where-Object -Property DisplayName -like "Mozilla Firefox*").UninstallString}
-    if ($String -eq $null) {Write-Host "Forefox is not installed" ; EXIT 0}
+    if ($String -eq $null) {Write-Host "Firefox is not installed" ; EXIT 0}
     $Pros = Start-Process $String -ArgumentList "/S" -Wait -PassThru
-    IF ($Pros.ExitCode -ne 0){EXIT $Pros}
-}
-
-Function Repair-App {
-    Uninstall-App
-    Install-App
+    $StopCode = $($Pros.ExitCode)
 }
 
 switch ($Action) {
     'Uninstall' {Uninstall-App}
-    'Repair' {Repair-App}
+    'Repair' {Uninstall-App;Install-App}
     Default {Install-app}
 }
+
+EXIT $StopCode
